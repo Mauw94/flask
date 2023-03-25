c52b8a8ccc9f9016433a867a476f1842119d6804
Maurits <mauritsseelen@hotmail.com>
2023-03-25 22:15:22 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+
+from model.income import Income, IncomeSchema
+from model.expense import Expense, ExpenseSchema
+from model.transaction_type import TransactionType
+
+
+# run .bootstrap.sh to start the api
+
+app = Flask(__name__)
+
+transactions = [
+    Income('Salary', 5000),
+    Income('Dividens', 200),
+    Expense('pizza', 50),
+    Expense('Rock concert', 100)
+]
+
+
+@app.route("/incomes")
+def get_incomes():
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions))
+
+    return jsonify(incomes)
+
+
+@app.route("/incomes", methods=['POST'])
+def add_income():
+    income = IncomeSchema().load(request.get_json())
+    transactions.append(income)
+
+    return '', 204
+
+
+@app.route('/expenses')
+def get_expenses():
+    schema = ExpenseSchema(many=True)
+    expenses = schema.dump(
+        filter(lambda t: t.type == TransactionType.EXPENSE, transactions))
+
+    return jsonify(expenses)
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense)
+
+    return '', 204
+
+
+if __name__ == "__main__":
+    app.run()
